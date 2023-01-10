47749902bcd909d5feac925ad4bb222e7a4dcb6d
Naww137 <57197000+Naww137@users.noreply.github.com>
2023-01-10 14:33:19 -0500

    Update example to fix max_parameter variable

--- run_adam.py
+++ run_adam.py
@@ -10,16 +10,19 @@ import os
 
 # number_of_pixels = 1936
 # number_of_pixels = 400
-number_of_pixels = 289
+# number_of_pixels = 289
 # number_of_pixels = 121
+number_of_pixels = 2
 
 ### Define geometric regions (repeating regions in this case) and the materials present within each
-region_definition = {'rod':['fuel','moderator'], 'gap':['moderator'], 'clad':['zircalloy','moderator']}
-# region_definition = {'whole_pixel':['fuel','moderator']}
-# region_definition = {'whole_pixel':['fuelmodmix']}
+### region_definition = {'rod':['fuel','moderator'], 'gap':['moderator'], 'clad':['zircalloy','moderator']}
+region_definition = {'left':['fuel'], 'right':['moderator']}
+### region_definition = {'whole_pixel':['fuel','moderator']}
+### region_definition = {'whole_pixel':['fuelmodmix']}
 
 ### Define the optimization parameters corresponding to the geometric region definition
-parameter_definition = {'rod':['theta0','theta1'], 'gap':['theta1'], 'clad':['theta0','theta1']}
+# parameter_definition = {'rod':['theta0','theta1'], 'gap':['theta1'], 'clad':['theta0','theta1']}
+parameter_definition = {'left':['theta0'], 'right':['theta1']}
 # parameter_definition = {'whole_pixel':['theta0','theta1']}
 # parameter_definition = {'whole_pixel':['theta0']}
 
@@ -29,11 +32,35 @@ material_dict_base = {
                                 'u-238':2.23686E-02,
                                 'o-16':4.64708E-02},
                 
+                        # 'zircalloy':{'o-16' : 2.71200E-04,
+                        #                 'cr-52':6.98800E-05,
+                        #                 'fe-56':1.42586E-04,
+                        #                 'fe-58':4.38228E-07,
+                        #                 'zr-94':7.37398E-03},
                         'zircalloy':{'o-16' : 2.71200E-04,
-                                        'cr-52':6.98800E-05,
-                                        'fe-56':1.42586E-04,
-                                        'fe-58':4.38228E-07,
-                                        'zr-94':7.37398E-03},
+                                'cr-50':3.62373E-06,
+                                'cr-52':6.98800E-05,
+                                'cr-53':7.92383E-06,
+                                'cr-54':1.97241E-06,
+                                'fe-54':9.08312E-06,
+                                'fe-56':1.42586E-04,
+                                'fe-57':3.29292E-06,
+                                'fe-58':4.38228E-07,
+                                'zr-90':2.18292E-02,
+                                'zr-91':4.76042E-03,
+                                'zr-92':7.27640E-03,
+                                'zr-94':7.37398E-03,
+                                'zr-96':1.18798E-03,
+                                'sn-112':4.64145E-06,
+                                'sn-114':3.15810E-06,
+                                'sn-115':1.62690E-06,
+                                'sn-116':6.95739E-05,
+                                'sn-117':3.67488E-05,
+                                'sn-118':1.15893E-04,
+                                'sn-119':4.11031E-05,
+                                'sn-120':1.55895E-04,
+                                'sn-122':2.21545E-05,
+                                'sn-124':2.77051E-05},
                         
                         'moderator':{
                                         'o-16':3.3368E-02,
@@ -58,13 +85,14 @@ material_dict_base = {
                                                                                 }
 
 # %% Initial parameters
-max_parameters = max(len(v) for k,v in parameter_definition.items())
+max_parameters = len(np.unique(np.array([(v) for k,v in parameter_definition.items()])))
+# max_parameters = max(len(v) for k,v in parameter_definition.items())
 initial_parameter_df = pd.DataFrame()
 for i in range(max_parameters):
     if i == 0:
-        initial_parameter_df[f'theta{i}'] = np.ones([number_of_pixels])*0.2
+        initial_parameter_df[f'theta{i}'] = np.ones([number_of_pixels])*0
     else:
-        initial_parameter_df[f'theta{i}'] = np.ones([number_of_pixels])*-4
+        initial_parameter_df[f'theta{i}'] = np.ones([number_of_pixels])*0
     initial_parameter_df[f'mt{i}'] = np.zeros([number_of_pixels])
     initial_parameter_df[f'vt{i}'] = np.zeros([number_of_pixels])
 
@@ -119,9 +147,10 @@ def obj_derivative(parameter_np, derivative_np, Nbase_np, keff):
 
 options = { 'Write Output'  :   True, 
             'Build Input'   :   True, 
-            'Submit Job'    :   True,
+            'Submit Job'    :   False,
             'Run Geometry Check' : False,
-            'Use Starting Fission Source' : True,
+            'Use Starting Fission Source' : False,
+            'Run Shift' : True ,
 
             'beta 1'    :   0.9, 
             'beta 2'    :   0.999,
@@ -167,7 +196,7 @@ else:
 
 
 ### Run ADAM 
-while step < 600:
+while step < 1000:
 
     keff = ADAM_control_module.run(step, pixel_array, pdef, output_filepath)
 
